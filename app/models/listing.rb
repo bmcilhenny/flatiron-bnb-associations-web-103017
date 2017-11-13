92f2a5566c68e2a07c376ee76b243c08594465da
github username <github email address>
2017-11-13 18:58:36 -0500

    done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ActiveRecord::Base
 
   belongs_to :host, :class_name => "User"
   belongs_to :neighborhood
+  has_many :reservations
+  has_many :reviews, through: :reservations#, :source => :guest
+  #has_many :guests, :class_name => "User"
+  has_many :guests, through: :reservations
 end
---
+++ db/migrate/07_add_listing.rb
@@ -0,0 +1,5 @@
+class AddListing < ActiveRecord::Migration
+  def change
+    add_column :users, :listing_id, :integer
+  end
+end
